a8d4ce84214d29c31f8abbfdbd633d9408e38ffa
ZnS-20 <mohda698@gmail.com>
2019-03-15 23:08:33 +0530

    Main class

--- Intermine_Schema_Validator.java
+++ Intermine_Schema_Validator.java
@@ -1,16 +1,16 @@
-import validate.Validate;	
+import validate.Validate;	//import the Validate class from validate package.
 
 import java.io.File;
 import java.util.Scanner;
 
 class Intermine_Schema_Validator
 {
-	public static void main(String[] args)throws Exception 
+	public static void main(String[] args)throws Exception
 	{
 		Scanner scan = new Scanner(System.in);
 		String st = scan.nextLine();
 		File file = new File(st);
-        Validate obj = new Validate();
+        Validate obj = new Validate(); //Validate class Constructer.
         obj.check_extension(file);
     }
 }
